874edb011481ce2ea5fba4405ce3e02554aeda9e
oseiasdfarias <oseias.dfarias@gmail.com>
2024-01-20 12:23:30 -0300

    novos arquivos add na pasta do curso de c02_...

--- 01_one_fase_selecao_t6/02_iniciante_em_programacao_t6_one/c01_logic_de_programacao_mergulhe_em_programacao_com_javascript/logica-js-projeto_inicial/app.js
+++ 01_one_fase_selecao_t6/02_iniciante_em_programacao_t6_one/c01_logic_de_programacao_mergulhe_em_programacao_com_javascript/logica-js-projeto_inicial/app.js
@@ -1,15 +1,16 @@
 alert("Boas vindas ao jogo dos números secretos!");
-let numeroSecreto = 3;
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 
+
 // enquanto o número não for igual ao n.s
 while (chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 0 e 10");
+    chute = prompt(`Escolha um número entre 0 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto
-               ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else{
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -19,3 +20,7 @@ while (chute != numeroSecreto) {
         tentativas ++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Isso ai! Você descobriu o número secreto
+               ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
