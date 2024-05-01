5294cc09a5ea3a471bd76a9f5467775f752b42e3
oseiasdfarias <oseias.dfarias@gmail.com>
2024-05-01 12:58:38 -0300

    feat: add project Currency Converter

---
+++ 02_one_fase_formacao_t6/Java_e_Spring_Framework_T6_ONE/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?i=tt3896198&apikey=8ff55a50");
+		System.out.println(json);
+	}
+}
